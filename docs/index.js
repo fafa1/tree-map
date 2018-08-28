baa713e37b5c908747a5c3456893b5606a43e374
Fagner <bfagner@hotmail.com>
2018-08-28 00:20:39 -0300

    forma correta de apresentação

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,12 @@
-$.getJSON('data.json', function (data) {
+$.getJSON('exemplo.json', function (data) {
 
-    var points = [],
+    let points = [],
         regionP,
         regionVal,
         regionI = 0,
+        mes,
+        mesP,
+        mesI,
         countryP,
         countryI,
         causeP,
@@ -28,6 +31,8 @@ $.getJSON('data.json', function (data) {
                 id: 'id_' + regionI,
                 name: region,
                 color: Highcharts.getOptions().colors[regionI]
+                //color: Highcharts.getOptions().colors[countryI]
+                
             };
             countryI = 0;
             for (country in data[region]) {
@@ -38,24 +43,36 @@ $.getJSON('data.json', function (data) {
                         parent: regionP.id
                     };
                     points.push(countryP);
-                    causeI = 0;
-                    for (cause in data[region][country]) {
-                        if (data[region][country].hasOwnProperty(cause)) {
-                            causeP = {
-                                id: countryP.id + '_' + causeI,
-                                name: causeName[cause],
-                                parent: countryP.id,
-                                value: (+data[region][country][cause])
-                            };
-                            regionVal += causeP.value;
-                            points.push(causeP);
-                            causeI = causeI + 1;
+                    mesI = 0
+                  for(mes in data[region][country]) {
+                      if(data[region][country].hasOwnProperty(mes)) {
+                        mesP = {
+                            id: countryP.id + '_' + mesI,
+                            name: mes,
+                            parent: countryP.id
                         }
-                    }
-                    countryI = countryI + 1;
+                        points.push(mesP)
+                        causeI = 0
+                        for (cause in data[region][country][mes]) {
+                            if (data[region][country][mes].hasOwnProperty(cause)) {
+                                causeP = {
+                                    id: mesP.id + '_' + causeI,
+                                    name: causeName[cause],
+                                    parent: mesP.id,
+                                    value: (+data[region][country][mes][cause])
+                                };
+                                regionVal += causeP.value;
+                                points.push(causeP);
+                                causeI = causeI + 1;
+                            }
+                        }
+                        mesI = mesI + 1;                   
+                      }
+                  }
+                  countryI = countryI + 1
                 }
             }
-            regionP.value = Math.round(regionVal / countryI);
+            regionP.value = Math.round(regionVal / (mesI + countryI));
             points.push(regionP);
             regionI = regionI + 1;
         }
@@ -80,7 +97,7 @@ $.getJSON('data.json', function (data) {
             data: points
         }],
         subtitle: {
-            text: 'Paciente: Veronica H'
+            text: 'Pacientes'
         },
         title: {
             text: 'Visualização de Dados na Saúde'
